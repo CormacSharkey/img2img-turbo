4484636e7af2b7fae7379990777a3c3f6063fd44
CormacSharkey <csharkey@tcd.ie>
2024-11-10 16:00:57 +0000

    updating project files

---
+++ exe_day.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/4.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/9.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/15.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/16.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/17.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/110.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/138.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/172.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/173.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/174.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/179.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/180.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/181.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/186.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/187.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/188.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/191.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/223.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/224.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/225.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/320.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/a.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/b.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/c.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/d.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/e.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/f.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/g.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/h.jpg" --output_dir "data_day_night/load_day/outputs"
+
+    python src/inference_unpaired.py --model_name "day_to_night" \
+    --input_image "data_day_night/load_day/inputs/i.jpg" --output_dir "data_day_night/load_day/outputs"
---
+++ exe_night.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+current_time=$(date +"%H:%M:%S")
+echo "Current time: $current_time"
+
+python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/2.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/10.jpg" --output_dir "data_day_night/load_night/outputs"
+
+python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/15.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/40.jpg" --output_dir "data_day_night/load_night/outputs"
+
+python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/42.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/53.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/74.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/101.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/105.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/112.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/114.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/131.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/GOPR086.png" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/GOPR0163.png" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/GOPR0421.png" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/GOPR0959.png" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/GP085.png" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/005211.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/004435.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/005144.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/a.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/b.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/c.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/d.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/e.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/f.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/g.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/h.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/i.jpg" --output_dir "data_day_night/load_night/outputs"
+
+    python src/inference_unpaired.py --model_name "night_to_day" \
+    --input_image "data_day_night/load_night/inputs/j.jpg" --output_dir "data_day_night/load_night/outputs"
+
+current_time=$(date +"%H:%M:%S")
+echo "Current time: $current_time"
