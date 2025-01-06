0f8a8f095280c8dd68b117c41d0dc53b88a9c4a6
CormacSharkey <csharkey@tcd.ie>
2025-01-06 10:07:29 +0000

    Christmas break - cleaning

---
+++ inference_scripts/trained_models_scripts_CURRENT/day_inference.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# inferences fake night images
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/4.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/9.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/15.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/16.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/17.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/110.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/138.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/172.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/173.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/174.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/179.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/180.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/181.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/186.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/187.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/188.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/191.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/223.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/224.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/225.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/320.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/a.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/b.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/c.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_day/inputs/d.jpg" --prompt " a cat eating a fish in the countryside" --direction "a2b" --output_dir "training_outputs/fake_night" 
---
+++ inference_scripts/trained_models_scripts_CURRENT/night_inference.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# inferences fake day images
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/2.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/10.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/15.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/40.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/42.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/53.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/74.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/101.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/105.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/112.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/114.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/131.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/GOPR086.png" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/GOPR0163.png" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/GOPR0421.png" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/GOPR0959.png" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/GP085.png" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/005211.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/004435.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/005144.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/a.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/b.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/c.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/d.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/e.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/f.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/g.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/h.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/i.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
+
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+--input_image "data_day_night/load_night/inputs/j.jpg" --prompt " a cat eating a fish in the countryside" --direction "b2a" --output_dir "training_outputs/fake_day" 
