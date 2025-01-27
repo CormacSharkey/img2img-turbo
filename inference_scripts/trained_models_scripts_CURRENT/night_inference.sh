af2e34c39fe90682e024f8363076737250e5dfc9
CormacSharkey <csharkey@tcd.ie>
2025-01-27 14:03:52 +0000

    updating after cleaning up file structure

--- inference_scripts/trained_models_scripts_CURRENT/night_inference.sh
+++ inference_scripts/trained_models_scripts_CURRENT/night_inference.sh
@@ -5,92 +5,92 @@
 cd ..
 cd ..
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/2.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/10.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/15.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/40.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/42.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/53.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/74.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/101.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/105.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/112.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/114.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/131.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/GOPR086.png" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/GOPR0163.png" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/GOPR0421.png" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/GOPR0959.png" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/GP085.png" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/005211.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/004435.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/005144.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/a.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/b.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/c.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/d.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/e.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/f.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/g.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/h.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/i.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
 
-python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_1000.pkl" \
+python src/inference_unpaired.py --model_path "training_outputs/checkpoints/model_5000.pkl" \
 --input_image "data_day_night/load_night/inputs/j.jpg" --prompt "Cityscape view at daytime" --direction "b2a" --output_dir "training_outputs/fake_day" 
--- src/train_cyclegan_turbo.py
+++ src/train_cyclegan_turbo.py
@@ -344,7 +344,7 @@ def main(args):
                                 torch.cuda.empty_cache()
 
                     # if global_step % args.checkpointing_steps == 1:
-                    if global_step % 1000 == 0:
+                    if global_step % 500 == 0:
                         outf = os.path.join(args.output_dir, "checkpoints", f"model_{global_step}.pkl")
                         sd = {}
                         sd["l_target_modules_encoder"] = l_modules_unet_encoder
